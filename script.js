a64a2ee8a181b09850b2d29664373dc8d1f1a01a
AzulaHana <201996070+AzulaHana@users.noreply.github.com>
2025-11-10 13:20:28 -0800

    Add switch statement to generate words instead of numbers

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function getComputerChoice(max) {
-    return Math.floor(Math.random() * 3);
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch (randomNumber) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors"
+    }
 }
-
 
 console.log(getComputerChoice(3))
